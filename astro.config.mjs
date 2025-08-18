31245b14559f3ab266617e36f36229bc64263c94
Jacob Traunero <jt7991@gmail.com>
2025-08-17 22:31:54 -0400

    Add faq

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,19 @@
 // @ts-check
-import tailwindcss from '@tailwindcss/vite';
-import { defineConfig, fontProviders } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig, fontProviders } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-				experimental: {
-        fonts: [{
-            provider: fontProviders.google(),
-            name: "Oswald",
-            cssVariable: "--font-roboto"
-        }]
-    }
+  experimental: {
+    fonts: [
+      {
+        provider: fontProviders.google(),
+        name: "Oswald",
+        cssVariable: "--font-roboto",
+      },
+    ],
+  },
 });
