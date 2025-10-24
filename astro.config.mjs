e2fdde8741dfc5d38a658e969a1f4b52bbf13682
Jacob Traunero <jt7991@gmail.com>
2025-10-23 21:44:14 -0400

    seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,17 @@
 // @ts-check
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig, fontProviders } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://richmondbarber.com",
+  integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()],
+    build: {
+      minify: true,
+    },
   },
   experimental: {
     fonts: [
